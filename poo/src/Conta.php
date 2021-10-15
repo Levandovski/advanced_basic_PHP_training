f301ffb2e4a62749726159224de3cde5d4d2dae4
jesse.levandovski@outlook.com <jesse.levandovski@outlook.com>
2021-10-15 00:25:57 -0300

    Nova implementação de script em php

--- poo/src/Conta.php
+++ poo/src/Conta.php
@@ -4,6 +4,42 @@ class Conta{
 
     public $cpfTitular;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
+
+    public function sacar(float $valorSacar){
+
+        if($valorSacar > $this->saldo){
+
+            echo "Saldo indisponivel";
+            return;
+        }
+
+        $this->saldo -= $valorSacar;
+     
+    }
+
+    public function depositar(float $valorDepositar): void {
+
+        if($valorDepositar < 0){
+            echo "Valor precisa ser positivo";
+            return;
+        }
+        
+        this->saldo += $valorDepositar;
+        
+    }
+
+    public function transferir(float $valorTransferir, $contaDestino): void {
+
+        if($valorTransferir > $this->saldo){
+            
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorTransferir);
+        $contaDestino->depositar($valorTransferir);
+
+    }
 
 }
